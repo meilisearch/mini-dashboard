14f07e96953a5ae7ec0b86756dcfc509d3446bf4
Morgane Dubus <morgane.d@meilisearch.com>
2021-03-17 16:33:22 +0100

    Add icons

---
+++ src/stories/Icons.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Box from 'components//Box'
+import MSLogo from 'components/icons/MSLogo'
+
+export default {
+  title: 'Components/Icons',
+}
+
+export const MeilisearchLogo = () => (
+  <Box>
+    <MSLogo size={64} />
+  </Box>
+)
