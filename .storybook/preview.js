574c1a2efb8913aaabe277a564de87bc6fcac367
Morgane Dubus <morgane.d@meilisearch.com>
2021-03-18 16:33:55 +0100

    Add themeprovider for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import React from 'react'
+import theme from 'theme'
+
+import { ThemeProvider } from 'styled-components'
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+]
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
