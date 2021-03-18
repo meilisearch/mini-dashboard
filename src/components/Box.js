744f8940b704b4b4abab36d605b468480ba5f1df
Morgane Dubus <morgane.d@meilisearch.com>
2021-03-18 16:18:11 +0100

    add flexbox to box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { space, layout, color, compose } from 'styled-system'
+import { space, layout, color, compose, flexbox } from 'styled-system'
 
-const Box = styled.div(compose(space, layout, color))
+const Box = styled.div(compose(space, layout, color, flexbox))
 
 export default Box
