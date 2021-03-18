bae78a447e205c7f31a6a0b3768a0e9a6d3aa88f
Morgane Dubus <morgane.d@meilisearch.com>
2021-03-18 16:16:19 +0100

    Add font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,8 +5,17 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
   * {
+    font-family: 'Work Sans';
     box-sizing: border-box;
   }
+
+  @font-face {
+    font-family: 'Work Sans';
+    src: url("fonts/Work_Sans/regular.woff2") format("woff2");
+    font-weight: 400;
+    font-style: normal;
+}
+
 `
 
 export default GlobalStyle
