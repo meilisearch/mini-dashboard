f6b62fab9fc7ecc5fbcbaa5910d3d7343d0bb8df
Morgane Dubus <morgane.d@meilisearch.com>
2021-04-08 17:14:37 +0200

    Prevent styled system props to be in the dom

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 import { space, layout, color, compose, flexbox, position } from 'styled-system'
+import { props as stprops } from '@styled-system/should-forward-prop'
 
-const Box = styled.div(compose(space, layout, color, flexbox, position))
+const regex = new RegExp(`^(${stprops.join('|')})$`)
 
+const shouldForwardProp = (prop) => !regex.test(prop)
+
+const Box = styled('div').withConfig({ shouldForwardProp })`
+  box-sizing: border-box;
+  min-width: 0;
+
+  && {
+    ${compose(space, layout, color, flexbox, position)}
+  }
+`
 export default Box
