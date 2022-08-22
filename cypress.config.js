da3d6e9eef2ba8ae6d36cd43cc16565666656a0e
Morgane Dubus <morgane.d@meilisearch.com>
2022-08-22 15:40:48 +0200

    Update cypress to version 10

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  env: {
+    host: 'http://0.0.0.0:7700',
+    apiKey: 'masterKey',
+    wrongApiKey: 'wrongApiKey',
+    waitingTime: 1000,
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
