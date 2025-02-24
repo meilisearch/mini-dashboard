e5558c95a89dbc12d25a577a091329fcac23483e
Strift <lau.cazanove@gmail.com>
2025-02-24 13:16:56 +0800

    Add side panel tests

---
+++ cypress/e2e/side-panel.cy.js
@@ -0,0 +1,20 @@
+describe(`Right side panel`, () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Should be opened by default', () => {
+    cy.get('[data-testid="right-panel"]').should('be.visible')
+  })
+
+  it('Should be closed when clicking on the close button', () => {
+    cy.get('button[aria-label="Close Panel"]').click()
+    cy.get('[data-testid="right-panel"]').should('not.be.visible')
+  })
+
+  it('Should be opened when clicking on the menu bars button', () => {
+    cy.get('button[aria-label="Close Panel"]').click()
+    cy.get('button[aria-label="Open Panel"]').click()
+    cy.get('[data-testid="right-panel"]').should('be.visible')
+  })
+})
