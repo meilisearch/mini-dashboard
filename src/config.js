0c99becc90f88a3f7181cd6662155d2c90bfd088
Strift <lau.cazanove@gmail.com>
2026-02-11 13:45:20 +0800

    Migrate from CRA to Vite

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const baseUrl =
-  process.env.REACT_APP_MEILI_SERVER_ADDRESS ||
-  (process.env.NODE_ENV === 'development'
-    ? 'http://0.0.0.0:7700'
-    : window.location.origin)
+  import.meta.env.VITE_MEILI_SERVER_ADDRESS ||
+  (import.meta.env.DEV ? 'http://0.0.0.0:7700' : window.location.origin)
 
 export default baseUrl
